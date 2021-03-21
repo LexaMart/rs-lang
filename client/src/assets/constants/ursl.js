ddc4c5df61b48010f85a45bd056448d07812b570
Alex <lexamart1708@gmail.com>
2021-03-21 14:29:47 +0300

    feat: add http hook

---
+++ client/src/assets/constants/ursl.js
@@ -0,0 +1,3 @@
+export const urls = {
+  API: 'https://rs-lang-api.herokuapp.com',
+}
--- client/src/pages/MainPage/Main.js
+++ client/src/pages/MainPage/Main.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import {useHttp} from './../../hooks/http.hook'
 export const Main = () => {
+
+  const {request} = useHttp()
+  const fetch = async () =>{ 
+  const fetched = await request('https://rs-lang-api.herokuapp.com/words', 'GET') 
+  console.log(fetched)
+  }
   return (
     <div>
       <h1> Main page</h1>
+      <button onClick={() => fetch()}>click</button>
     </div>
   )
 }
