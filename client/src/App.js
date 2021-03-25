27f01cc6c0759369408dfd9a198d5fe545b9f885
Nerbet <gdruzhina@mail.ru>
2021-03-25 20:06:15 +0300

    add: add layot

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import 'materialize-css';
-
 import { useRoutes } from './routes';
+import Header from './pages/Header/Header';
+import Footer from './pages/Footer/Footer';
 
 function App() {
   const routes = useRoutes(true)
   return (
+    <div className="app_container">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+    <Header />
     <Router>
-      <div className="container">
+      <div className="main_container">
         {routes}
       </div>
     </Router>
+    <Footer />
+    </div>
   );
 }
 
