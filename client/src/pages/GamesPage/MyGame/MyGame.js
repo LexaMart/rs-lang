965f7358f88ae8b69c5c0edb4942f638d063aee1
Alex <lexamart1708@gmail.com>
2021-03-27 20:11:42 +0300

    feat: add request with learned words

--- client/src/pages/GamesPage/MyGame/MyGame.js
+++ client/src/pages/GamesPage/MyGame/MyGame.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import { useHttp } from '../../../hooks/http.hook';
 
 import urls from '../../../assets/constants/ursl'
@@ -6,6 +7,8 @@ import './myGame.scss';
 import { Rules } from './components/Rules';
 
 export const MyGame = () => {
+  const token = useSelector((store) => store.authStore.userData.token);
+  const userId = useSelector((store) => store.authStore.userData.userId);
   const { request } = useHttp()
   const [isGameStarted, setIsGameStarted] = useState(false)
   const [isGameLost, setIsGameLost] = useState(false)
@@ -13,18 +16,25 @@ export const MyGame = () => {
   const [winingCard, setWiningCard] = useState(null)
   const [gameCards, setGameCards] = useState([])
   const [lives, setLives] = useState([])
+  const [isFirtsTry, setFirstTry] = useState(true);
 
   const startGame = async () => {
     setLives([1, 1, 1, 1])
     setIsGameStarted(true)
     setRandomInt([Math.floor(Math.random() * 6), Math.floor(Math.random() * 29)])
   }
-  const choiceHandler = (e) => {
-    console.log(e.target.src)
-    console.log(`${urls.API}/${winingCard.image}`)
+  const choiceHandler = async (e) => {
     if (e.target.src === `${urls.API}/${winingCard.image}`) {
       console.log("WIN")
-      // setIsGameStarted(false)
+      if (isFirtsTry) {
+        await request(`${urls.API}/users/${userId}/words/${winingCard.id}`, "POST", {
+          "difficulty": "learned",
+        }, {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        })
+      }
       setRandomInt([Math.floor(Math.random() * 6), Math.floor(Math.random() * 29)])
     }
     else {
@@ -32,28 +42,22 @@ export const MyGame = () => {
         setIsGameLost(true)
         setIsGameStarted(false)
       }
-      console.log(lives)
+      setFirstTry(false);
       setLives(lives.splice(0, (lives.length - 1)))
-      console.log(lives)
     }
   }
   useEffect(useCallback(async () => {
-    console.log(gameCards)
     if (isGameStarted) {
       const arrayToPlay = [];
-      // setRandomInt([Math.floor(Math.random() * 6), Math.floor(Math.random() * 29)])
-      // const randCard = Math.floor(Math.random() * 6);
-      // const randPage = Math.floor(Math.random() * 29)
       const cards = await request(`${urls.API}/words?group=${randomInt[0]}&page=${randomInt[1]}`, "GET")
       for (let i = 0; i <= 3; i++) {
         let randChoice = (Math.floor(Math.random() * (cards.length - 1)))
         arrayToPlay.push(cards[randChoice]);
         cards.splice(randChoice, 1);
       }
-      console.log(arrayToPlay)
       setGameCards(arrayToPlay)
       setWiningCard(arrayToPlay[Math.floor(Math.random() * 4)])
-
+      setFirstTry(true)
     }
   }, [isGameStarted, gameCards, request, randomInt]), [isGameStarted, randomInt])
   return (
