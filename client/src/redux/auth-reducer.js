b8312c7e364fc53ee16350d53e6d9ecd771ffd63
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-05 19:25:55 +0300

    feat: add user words to redux

--- client/src/redux/auth-reducer.js
+++ client/src/redux/auth-reducer.js
@@ -4,18 +4,25 @@ const ACTION_CONST = {
   SET_USER_DATA: "SET_USER_DATA",
   SET_DEFAULT_VALUES: "SET_DEFAULT_VALUES",
   SET_IS_LOADING_PROGRESS: "AUTH_REDUCER_TOGGLE_IS_LOADING_PROGRESS",
+  SET_USER_LEARNING_WORDS: " SET_USER_LEARNING_WORDS",
+  SET_USER_HARD_WORDS: "SET_USER_HARD_WORDS",
+  SET_USER_DELETED_WORDS: "SET_USER_DELETED_WORDS",
 };
 
 export const DEFAULT_VALUES = {
   EMPTY: "",
   TRUE: true,
   FALSE: false,
+  EMPTY_ARRAY: [],
 };
 
 let initialState = {
   userData: DEFAULT_VALUES.EMPTY,
   isAuthorized: DEFAULT_VALUES.FALSE,
   isLoading: DEFAULT_VALUES.FALSE,
+  userLearningWords: DEFAULT_VALUES.EMPTY_ARRAY,
+  userHardWords: DEFAULT_VALUES.EMPTY_ARRAY,
+  userDeletedWords: DEFAULT_VALUES.EMPTY_ARRAY,
 };
 
 export const authReducer = (state = initialState, action) => {
@@ -37,6 +44,15 @@ export const authReducer = (state = initialState, action) => {
     case ACTION_CONST.SET_IS_LOADING_PROGRESS: {
       return { ...state, isLoading: action.isLoading };
     }
+    case ACTION_CONST.SET_USER_LEARNING_WORDS: {
+      return { ...state, userLearningWords: action.arrLearnWords };
+    }
+    case ACTION_CONST.SET_USER_HARD_WORDS: {
+      return { ...state, userHardWords: action.arrHardWords };
+    }
+    case ACTION_CONST.SET_USER_DELETED_WORDS: {
+      return { ...state, userDeletedWords: action.arrDeletedWords };
+    }
     default:
       return state;
   }
@@ -50,6 +66,21 @@ export const setUserData = (userData) => ({
 export const setIsLoadingInProgress = (isLoading) => ({
   type: ACTION_CONST.SET_IS_LOADING_PROGRESS,
   isLoading,
+});
+
+export const setUserLearningWords = (arrLearnWords) => ({
+  type: ACTION_CONST.SET_USER_LEARNING_WORDS,
+  arrLearnWords,
+});
+
+export const setUserHardWords = (arrHardWords) => ({
+  type: ACTION_CONST.SET_USER_HARD_WORDS,
+  arrHardWords,
+});
+
+export const setUserDeletedWords = (arrDeletedWords) => ({
+  type: ACTION_CONST.SET_USER_DELETED_WORDS,
+  arrDeletedWords,
 });
 
 export const logout = () => ({ type: ACTION_CONST.SET_DEFAULT_VALUES });
