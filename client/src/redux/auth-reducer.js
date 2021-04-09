4fe2f8805ec8968bd3bc5b2e099c6fc0ae0c9af5
Alex <lexamart1708@gmail.com>
2021-04-09 21:55:19 +0300

    fix: statistic fix

--- client/src/redux/auth-reducer.js
+++ client/src/redux/auth-reducer.js
@@ -1,23 +1,16 @@
-import { rsLangApi } from '../services/rs-lang-api';
-import { getWords } from '../services/getAllWords';
+import { rsLangApi } from "../services/rs-lang-api";
 
 const ACTION_CONST = {
-  SET_USER_DATA: 'SET_USER_DATA',
-  SET_DEFAULT_VALUES: 'SET_DEFAULT_VALUES',
-  SET_IS_LOADING_PROGRESS: 'AUTH_REDUCER_TOGGLE_IS_LOADING_PROGRESS',
-  SET_USER_LEARNING_WORDS: ' SET_USER_LEARNING_WORDS',
-  SET_USER_HARD_WORDS: 'SET_USER_HARD_WORDS',
-  SET_USER_DELETED_WORDS: 'SET_USER_DELETED_WORDS',
-  ADD_DELETED_WORD: 'ADD_DELETED_WORD',
-  REMOVE_DELETED_WORD: 'REMOVE_DELETED_WORD',
-  ADD_HARD_WORD: 'ADD_HARD_WORD',
-  REMOVE_HARD_WORD: 'REMOVE_HARD_WORD',
-  ADD_LEARNING_WORD: 'ADD_LEARNING_WORD',
-  REMOVE_LEARNING_WORD: 'REMOVE_LEARNING_WORD',
+  SET_USER_DATA: "SET_USER_DATA",
+  SET_DEFAULT_VALUES: "SET_DEFAULT_VALUES",
+  SET_IS_LOADING_PROGRESS: "AUTH_REDUCER_TOGGLE_IS_LOADING_PROGRESS",
+  SET_USER_LEARNING_WORDS: " SET_USER_LEARNING_WORDS",
+  SET_USER_HARD_WORDS: "SET_USER_HARD_WORDS",
+  SET_USER_DELETED_WORDS: "SET_USER_DELETED_WORDS",
 };
 
 export const DEFAULT_VALUES = {
-  EMPTY: '',
+  EMPTY: "",
   TRUE: true,
   FALSE: false,
   EMPTY_ARRAY: [],
@@ -60,39 +53,6 @@ export const authReducer = (state = initialState, action) => {
     case ACTION_CONST.SET_USER_DELETED_WORDS: {
       return { ...state, userDeletedWords: action.arrDeletedWords };
     }
-    case ACTION_CONST.ADD_DELETED_WORD: {
-      const stagedUserDeletedWords = state.userDeletedWords.slice();
-      stagedUserDeletedWords.push(action.activeWordId);
-      return { ...state, userDeletedWords: stagedUserDeletedWords };
-    }
-    case ACTION_CONST.REMOVE_DELETED_WORD: {
-      const stagedUserDeletedWords = state.userDeletedWords.filter(
-        (item) => item !== action.activeWordId
-      );
-      return { ...state, userDeletedWords: stagedUserDeletedWords };
-    }
-    case ACTION_CONST.ADD_HARD_WORD: {
-      const stagedUserHardWords = state.userHardWords.slice();
-      stagedUserHardWords.push(action.activeWordId);
-      return { ...state, userHardWords: stagedUserHardWords };
-    }
-    case ACTION_CONST.REMOVE_HARD_WORD: {
-      const stagedUserHardWords = state.userHardWords.filter(
-        (item) => item !== action.activeWordId
-      );
-      return { ...state, userHardWords: stagedUserHardWords };
-    }
-    case ACTION_CONST.ADD_LEARNING_WORD: {
-      const stagedUserLearningWords = state.userLearningWords.slice();
-      stagedUserLearningWords.push(action.activeWordId);
-      return { ...state, userLearningWords: stagedUserLearningWords };
-    }
-    case ACTION_CONST.REMOVE_LEARNING_WORD: {
-      const stagedUserLearningWords = state.userLearningWords.filter(
-        (item) => item !== action.activeWordId
-      );
-      return { ...state, userLearningWords: stagedUserLearningWords };
-    }
     default:
       return state;
   }
@@ -123,51 +83,14 @@ export const setUserDeletedWords = (arrDeletedWords) => ({
   arrDeletedWords,
 });
 
-export const addDeletedWord = (activeWordId) => ({
-  type: ACTION_CONST.ADD_DELETED_WORD,
-  activeWordId,
-});
-export const removeDeletedWord = (activeWordId) => ({
-  type: ACTION_CONST.REMOVE_DELETED_WORD,
-  activeWordId,
-});
-
-export const addHardWord = (activeWordId) => ({
-  type: ACTION_CONST.ADD_HARD_WORD,
-  activeWordId,
-});
-export const removeHardWord = (activeWordId) => ({
-  type: ACTION_CONST.REMOVE_HARD_WORD,
-  activeWordId,
-});
-
-export const addLearningWord = (activeWordId) => ({
-  type: ACTION_CONST.ADD_LEARNING_WORD,
-  activeWordId,
-});
-export const removeLearningWord = (activeWordId) => ({
-  type: ACTION_CONST.REMOVE_LEARNING_WORD,
-  activeWordId,
-});
 export const logout = () => ({ type: ACTION_CONST.SET_DEFAULT_VALUES });
+
 export const login = (email, password) => async (dispatch) => {
   dispatch(setIsLoadingInProgress(true));
   const response = await rsLangApi.login(email, password);
-  const words = {
-    learn: true,
-    hard: true,
-    deleted: true,
-  };
   if (response) {
     dispatch(setUserData(response.data));
-    await getWords(response.data.token, response.data.userId, words).then(
-      (res) => {
-        dispatch(setUserLearningWords(res.learn));
-        dispatch(setUserHardWords(res.hard));
-        dispatch(setUserDeletedWords(res.deleted));
-      }
-    );
-  } else alert('Wrong email or password');
+  } else alert("Wrong email or password")
 
   dispatch(setIsLoadingInProgress(false));
 };
