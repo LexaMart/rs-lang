3a5192aa41fb53a0f0024622d74a928b009c441c
burik84 <avburik84@gmail.com>
2021-04-09 10:45:27 +0300

    feat: add navlink on modal statistic page to other pages

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import 'materialize-css';
 import '../pages/MainPage/popup.scss';
@@ -8,7 +9,7 @@ const Popup = ({ active, setActive, text, page, language = 'en' }) => {
   return (
     <div
       className={active ? 'popup popup_active' : 'popup'}
-      onClick={() => setActive(false)}
+      // onClick={() => setActive(false)}
     >
       <div
         className={
@@ -19,10 +20,22 @@ const Popup = ({ active, setActive, text, page, language = 'en' }) => {
         onClick={(e) => e.stopPropagation()}
       >
         <div className="word_tex popup__title">{text}</div>
+        <div className="info_container">
+          <div className="text_container">
+            <div className="text_meaning popup__description">
+              {language === 'en'
+                ? 'Go to main or to games'
+                : 'Перейдите на главную страницу или к играм'}
+            </div>
+          </div>
+        </div>
         <div className="button_container">
-          <button onClick={() => setActive(false)} className="btn">
-            {language === 'en' ? 'Close' : 'Закрыть'}
-          </button>
+          <NavLink className="btn" to="/main">
+            {language === 'en' ? 'Main' : 'Главная'}
+          </NavLink>
+          <NavLink className="btn" to="/games">
+            {language === 'en' ? 'Games' : 'Игры'}
+          </NavLink>
         </div>
       </div>
     </div>
