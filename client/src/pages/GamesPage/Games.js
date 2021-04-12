123ad8815385a1fccba3bce81bc9bee4b05efab4
burik84 <avburik84@gmail.com>
2021-04-13 00:52:29 +0300

    feat: add translate page games

--- client/src/pages/GamesPage/Games.js
+++ client/src/pages/GamesPage/Games.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentPage } from '../../redux/settings-reducer';
 
 import savannah from '../../assets/images/lion.svg';
@@ -8,34 +8,46 @@ import audio from '../../assets/images/audio.svg';
 import joystick from '../../assets/images/joystick.svg';
 import sprint from '../../assets/images/sprint.svg';
 import './games.scss';
-import { CURRENT_PAGE_NAME } from '../../shared/words-config';
-
+import {
+  CURRENT_PAGE_NAME,
+  LANGUAGE_CONFIG,
+  WORDS_CONFIG,
+} from '../../shared/words-config';
 
 export const Games = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const language = useSelector((store) => store.settingsStore.activeLanguage);
 
   useEffect(() => {
-    dispatch(setCurrentPage(CURRENT_PAGE_NAME.GAMES))
-  })
+    dispatch(setCurrentPage(CURRENT_PAGE_NAME.GAMES));
+  });
 
   return (
     <div className="games-block">
-      <NavLink className="game-card" to='/savannah'>
+      <NavLink className="game-card" to="/savannah">
         <img className="game-icon" src={savannah} alt="savannah" />
-        <span className="game-name">Savannah</span>
+        <span className="game-name">
+          {language === LANGUAGE_CONFIG.foreign ? 'Savannah' : 'Саванна'}
+        </span>
       </NavLink>
       <NavLink to="/audiocall" className="game-card">
         <img className="game-icon" src={audio} alt="audio" />
-        <span className="game-name">Auido game</span>
+        <span className="game-name">
+          {language === LANGUAGE_CONFIG.foreign ? 'Audio game' : 'Аудио игра'}
+        </span>
       </NavLink>
       <NavLink to="/mygame" className="game-card">
         <img className="game-icon" src={joystick} alt="my_game" />
-        <span className="game-name">Our game</span>
+        <span className="game-name">
+          {language === LANGUAGE_CONFIG.foreign ? 'Our game' : 'Наша игра'}
+        </span>
       </NavLink>
       <NavLink to="/sprint" className="game-card">
         <img className="game-icon" src={sprint} alt="sprint" />
-        <span className="game-name">Sprint</span>
+        <span className="game-name">
+          {language === LANGUAGE_CONFIG.foreign ? 'Sprint' : 'Спринт'}
+        </span>
       </NavLink>
     </div>
-  )
-}
+  );
+};
