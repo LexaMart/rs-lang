9fc3a80ac967d419b713f73b07cfef7aebeb591c
burik84 <avburik84@gmail.com>
2021-04-13 01:05:52 +0300

    feat: add translate page sprint

--- client/src/shared/words-config.js
+++ client/src/shared/words-config.js
@@ -294,4 +294,10 @@ export const WORDS_CONFIG = {
     foreign: 'Game is over your score is',
     native: 'Игра окончена, ваш счет',
   },
+  SPRINT_PAGE_RULES: {
+    foreign:
+      'In this game you have one minute to choose as musch correct answers as possible. If you answer right on the first time your this word wiil be marked as learned',
+    native:
+      'В этой игре у вас есть одна минута, чтобы выбрать как можно больше правильных ответов. Если вы ответите правильно с первого раза это слово будет помечено как выученное',
+  },
 };
