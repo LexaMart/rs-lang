ae1356b16dd4ccf32b330cdd9fb26dca6d41afd5
burik84 <avburik84@gmail.com>
2021-04-12 23:22:42 +0300

    feat: add translate on promo page

--- client/src/pages/PromoPage/Promo.js
+++ client/src/pages/PromoPage/Promo.js
@@ -1,90 +1,221 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentPage } from '../../redux/settings-reducer';
+import {
+  CURRENT_PAGE_NAME,
+  LANGUAGE_CONFIG,
+  WORDS_CATEGORIES,
+  WORDS_CONFIG,
+} from '../../shared/words-config';
 
 import { Link } from 'react-router-dom';
 import statistics from '../../assets/images/statistics.png';
 import Settings from '../../assets/images/settings.svg';
 import Joystick from '../../assets/images/joystick.svg';
 import Audio from '../../assets/images/audio.svg';
-import Picture from "../../assets/images/promo_img.png";
+import Picture from '../../assets/images/promo_img.png';
 import './promo.scss';
 
 export const Promo = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const language = useSelector((store) => store.settingsStore.activeLanguage);
+
   useEffect(() => {
-    dispatch(setCurrentPage('promo'))
-  })
+    dispatch(setCurrentPage('promo'));
+  });
   return (
     <div className="promo_container">
-      <div className="caption_promo white-text">Начни изучать английский язык вместе с RS Lang</div>
+      <div className="caption_promo white-text">
+        {language === LANGUAGE_CONFIG.foreign
+          ? WORDS_CONFIG.PROMO_PAGE_CAPTION.foreign
+          : WORDS_CONFIG.PROMO_PAGE_CAPTION.native}
+      </div>
       <div className="promo_first_block">
-        <div className="info_promo">Приложение для изучения иностранных слов с техникой отслеживания индивидуального прогресса, мини-играми и словарем на 3600 слов, для каждого из которых дана его транскрипция, пример применения и многое другое.</div>
-        <div><img src={Picture} alt="img" className="head_promo_img" /></div>
+        <div className="info_promo">
+          {language === LANGUAGE_CONFIG.foreign
+            ? WORDS_CONFIG.PROMO_PAGE_INFO.foreign
+            : WORDS_CONFIG.PROMO_PAGE_INFO.native}
+        </div>
+        <div>
+          <img src={Picture} alt="img" className="head_promo_img" />
+        </div>
       </div>
       <div className="youtube_video_container">
-        <iframe classname="youtube_video" width="560" height="315" src="https://www.youtube.com/embed/-043FORA-Gc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          classname="youtube_video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/-043FORA-Gc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
       <div className="feature_card card">
         <div className="feature_info">
-          <div className="head_card">Игры</div>
-          <div>В нашем приложении доступно 4 игры которые помогут проверить и закрепить изученный материал</div>
+          <div className="head_card">
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.GAMES_PAGE.foreign
+              : WORDS_CONFIG.GAMES_PAGE.native}
+          </div>
+          <div>
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_GAMES_INFO.foreign
+              : WORDS_CONFIG.PROMO_PAGE_GAMES_INFO.native}
+          </div>
         </div>
-        <div><img src={Joystick} alt="games" className="feature_img" /></div>
+        <div>
+          <img src={Joystick} alt="games" className="feature_img" />
+        </div>
       </div>
       <div className="feature_card card">
         <div className="feature_info">
-          <div className="head_card">Статистика</div>
-          <div>Просматривай и отслеживай свой прогресс</div>
+          <div className="head_card">
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.STATISTICS_PAGE.foreign
+              : WORDS_CONFIG.STATISTICS_PAGE.native}
+          </div>
+          <div>
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_STATISTICS_INFO.foreign
+              : WORDS_CONFIG.PROMO_PAGE_STATISTICS_INFO.native}
+          </div>
         </div>
-        <div><img src={statistics} alt="statistics" className="feature_img" /></div>
+        <div>
+          <img src={statistics} alt="statistics" className="feature_img" />
+        </div>
       </div>
       <div className="feature_card card">
         <div className="feature_info">
-          <div className="head_card">Настройки сложности</div>
-          <div>Настрой под себя сложность выбери с какой группы начать а также показывать ли тебе сложные переводы</div>
+          <div className="head_card">
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_SETTINGS.foreign
+              : WORDS_CONFIG.PROMO_PAGE_SETTINGS.native}
+          </div>
+          <div>
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_SETTINGS_INFO.foreign
+              : WORDS_CONFIG.PROMO_PAGE_SETTINGS_INFO.native}
+          </div>
         </div>
-        <div><img src={Settings} alt="settings" className="feature_img" /></div>
+        <div>
+          <img src={Settings} alt="settings" className="feature_img" />
+        </div>
       </div>
       <div className="feature_card card">
         <div className="feature_info">
-          <div className="head_card">Произношение</div>
-          <div>Прослушивай как правильно произносить слова и тренируйся</div>
+          <div className="head_card">
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.foreign
+              : WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.native}
+          </div>
+          <div>
+            {language === LANGUAGE_CONFIG.foreign
+              ? WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION_INFO.foreign
+              : WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION_INFO.native}
+          </div>
         </div>
-        <div><img src={Audio} alt="pronounsiation" className="feature_img" /></div>
+        <div>
+          <img src={Audio} alt="pronounsiation" className="feature_img" />
+        </div>
       </div>
       <div className="info_block_promo card white-text">
         <div className="disadvantages card">
           <div className="disadvantages_title card">
-            <div className="title_text">Without registration</div>
+            <div className="title_text">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.without
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.without}
+            </div>
           </div>
           <div className="disadvantages_entity card">
-            <div className="granted_point">Dictionary of 3600 words</div>
-            <div className="granted_point">Pronunciation</div>
-            <div className="granted_point">Games</div>
-            <div className="granted_point">Settings</div>
-            <div className="nongranted_point">Your own dictionary</div>
-            <div className="nongranted_point">Your own statistics</div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.words
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.words}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.foreign
+                : WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.native}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.GAMES_PAGE.foreign
+                : WORDS_CONFIG.GAMES_PAGE.native}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.SETTINGS.foreign
+                : WORDS_CONFIG.SETTINGS.native}
+            </div>
+            <div className="nongranted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.dictionary
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.dictionary}
+            </div>
+            <div className="nongranted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.statistic
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.statistic}
+            </div>
           </div>
         </div>
         <div className="advantages card">
           <div className="advantages_title card">
-            <div className="title_text">With registration</div>
+            <div className="title_text">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.with
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.with}
+            </div>
           </div>
           <div className="advantages_entity card">
-            <div className="granted_point">Dictionary of 3600 words</div>
-            <div className="granted_point">Pronunciation</div>
-            <div className="granted_point">Games</div>
-            <div className="granted_point">Settings</div>
-            <div className="granted_point">Your own dictionary</div>
-            <div className="granted_point">Your own statistics</div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.words
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.words}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.foreign
+                : WORDS_CONFIG.PROMO_PAGE_PRONUNCIATION.native}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.GAMES_PAGE.foreign
+                : WORDS_CONFIG.GAMES_PAGE.native}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.SETTINGS.foreign
+                : WORDS_CONFIG.SETTINGS.native}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.dictionary
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.dictionary}
+            </div>
+            <div className="granted_point">
+              {language === LANGUAGE_CONFIG.foreign
+                ? WORDS_CONFIG.PROMO_PAGE_BLOCK.foreign.statistic
+                : WORDS_CONFIG.PROMO_PAGE_BLOCK.native.statistic}
+            </div>
           </div>
         </div>
       </div>
       <div className="btn_container">
-        <Link className="btn sign_in_btn red" to="/login">Войти </Link>
-        <Link className="btn sign_in_btn" to="/registration">Регистрация </Link>
+        <Link className="btn sign_in_btn red" to="/login">
+          {language === LANGUAGE_CONFIG.foreign
+            ? WORDS_CONFIG.LOGIN_BUTTON.foreign
+            : WORDS_CONFIG.LOGIN_BUTTON.native}{' '}
+        </Link>
+        <Link className="btn sign_in_btn" to="/registration">
+          {language === LANGUAGE_CONFIG.foreign
+            ? WORDS_CONFIG.REGISTER_BUTTON.foreign
+            : WORDS_CONFIG.REGISTER_BUTTON.native}{' '}
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
