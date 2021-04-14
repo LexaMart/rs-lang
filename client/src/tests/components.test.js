b45fa75f6137337912099808caa1cdd4ba976581
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-14 22:33:16 +0300

    fix: change description

--- client/src/tests/components.test.js
+++ client/src/tests/components.test.js
@@ -5,22 +5,6 @@ import WordCard from '../pages/MainPage/WordCard'
 import { mount, render, shallow } from 'enzyme'
 import { Score } from '../pages/GamesPage/Sprint/components/Score/Score'
 
-const wordMock = {
-  "id": "5e9f5ee35eb9e72bc21af4a0",
-  "group": 0,
-  "page": 0,
-  "word": "alcohol",
-  "image": "files/01_0002.jpg",
-  "audio": "files/01_0002.mp3",
-  "audioMeaning": "files/01_0002_meaning.mp3",
-  "audioExample": "files/01_0002_example.mp3",
-  "textMeaning": "<i>Alcohol</i> is a type of drink that can make people drunk.",
-  "textExample": "A person should not drive a car after he or she has been drinking <b>alcohol</b>.",
-  "transcription": "[ǽlkəhɔ̀ːl]",
-  "textExampleTranslate": "Человек не должен водить машину после того, как он выпил алкоголь",
-  "textMeaningTranslate": "Алкоголь - это тип напитка, который может сделать людей пьяными",
-  "wordTranslate": "алкоголь"
-}
 const anyScore = '20';
 
 const setUpHeader = (props) => shallow(<Header {...props} />)
