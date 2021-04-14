ff87eb7ce835a6763a9ed40bbed7bbe9b0ded73b
Alex <lexamart1708@gmail.com>
2021-04-14 21:27:43 +0300

    feat: add video link

--- client/src/pages/PromoPage/Promo.js
+++ client/src/pages/PromoPage/Promo.js
@@ -40,7 +40,7 @@ export const Promo = () => {
           className="youtube_video"
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/-043FORA-Gc"
+          src="https://www.youtube.com/embed/PrLlIrSyhV8"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
